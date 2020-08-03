4ec04d3a81236a900673ccae1bd7ad553f4e2eee
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-08-03 12:32:03 +0300

    Finished Part-3

--- abp-tutorial/mvc-ef/source-code/src/Acme.BookStore.Web/Pages/Books/Index.js
+++ abp-tutorial/mvc-ef/source-code/src/Acme.BookStore.Web/Pages/Books/Index.js
@@ -2,6 +2,7 @@
     var l = abp.localization.getResource('BookStore');
 
     var createModal = new abp.ModalManager(abp.appPath + 'Books/CreateModal');
+    var editModal = new abp.ModalManager(abp.appPath + 'Books/EditModal');
 
     var dataTable = $('#BooksTable').DataTable(
         abp.libs.datatables.normalizeConfiguration({
@@ -12,6 +13,34 @@
             scrollX: true,
             ajax: abp.libs.datatables.createAjax(acme.bookStore.books.book.getList),
             columnDefs: [
+                {
+                    title: l('Actions'),
+                    rowAction: {
+                        items:
+                        [
+                            {
+                                text: l('Edit'),
+                                action: function (data) {
+                                    editModal.open({ id: data.record.id });
+                                }
+                            },
+                            {
+                                text: l('Delete'),
+                                confirmMessage: function (data) {
+                                    return l('BookDeletionConfirmationMessage', data.record.name);
+                                },
+                                action: function (data) {
+                                    acme.bookStore.books.book
+                                        .delete(data.record.id)
+                                        .then(function () {
+                                            abp.notify.info(l('SuccessfullyDeleted'));
+                                            dataTable.ajax.reload();
+                                        });
+                                }
+                            }
+                        ]
+                    }
+                },
                 {
                     title: l('Name'),
                     data: "name"
@@ -53,6 +82,10 @@
     );
 
     createModal.onResult(function () {
+        dataTable.ajax.reload();
+    });
+
+    editModal.onResult(function () {
         dataTable.ajax.reload();
     });
 
