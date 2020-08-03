25f4bc5031bceb9793bad35736696082e1f4b1d8
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-08-03 14:26:30 +0300

    Completed part-5.

--- abp-tutorial/mvc-ef/source-code/src/Acme.BookStore.Web/Pages/Books/Index.js
+++ abp-tutorial/mvc-ef/source-code/src/Acme.BookStore.Web/Pages/Books/Index.js
@@ -20,12 +20,14 @@
                         [
                             {
                                 text: l('Edit'),
+                                visible: abp.auth.isGranted('BookStore.Books.Edit'),
                                 action: function (data) {
                                     editModal.open({ id: data.record.id });
                                 }
                             },
                             {
                                 text: l('Delete'),
+                                visible: abp.auth.isGranted('BookStore.Books.Delete'),
                                 confirmMessage: function (data) {
                                     return l('BookDeletionConfirmationMessage', data.record.name);
                                 },
